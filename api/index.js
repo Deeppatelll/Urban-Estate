479bee970ce3877db9abb482da57e67c8c31c99b
Deeppatelll <deeppatel05082005@gmail.com>
2025-02-09 11:52:33 +0530

    Connect To Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
-const app=express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+// Fixed the connection string by URL-encoding the password
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log(err);
+});
 
-app.listen(4000, () =>{
-    console.log('server is running on port 4000');
-}
-);
+
+const app = express();
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
