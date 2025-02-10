2c0fa86f5dcfbfc425dadc1860067e0ba7423a20
Deeppatelll <deeppatel05082005@gmail.com>
2025-02-10 20:04:52 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import UserRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 // Fixed the connection string by URL-encoding the password
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,8 +13,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
 });
 app.use('/api/user',UserRouter);
+app.use('/api/auth',authRouter);
