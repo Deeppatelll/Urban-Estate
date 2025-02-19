b0a19fb66733cd5119003b2a066ab1c075b4d39c
Deeppatelll <deeppatel05082005@gmail.com>
2025-02-19 15:35:35 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,6 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
+
 
 export const createListing=async(req,res,next)=>{
     try{
@@ -23,3 +25,23 @@ try {
     next(error);   
  }
  };
+export const updateListing = async (req, res, next) => {
+    try {
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return next(errorHandler(404, "Listing not found!"));
+        }
+        if (req.user.id !== listing.userRef.toString()) {
+            return next(errorHandler(401, "You can only update your own listings!"));
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        return next(errorHandler(404, "Listing not found!"));
+    }
+};
